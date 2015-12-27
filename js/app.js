84f28c1a60de147e5c9dee01a410e8e551e947a5
chels <cstauber77@gmail.com>
2015-12-26 19:31:53 -0700

    Add bugs. Move princess.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.sprite = 'images/enemy-bug.png';
+    this.speed = speed;
+    this.x = x;
+    this.y = y;
+    this.initialX = x;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +19,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if(this.x > 500){
+        this.x = this.initialX;
+    }
+    else{
+        this.x = this.x + this.speed*dt;
+    }
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +36,42 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y){
+    this.sprite = 'images/char-princess-girl.png';
+    this.x = x;
+    this.y = y;
+    this.initialX = x;
+    this.initialY = y;
+};
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.handleInput = function(input) {
+    if(input === 'left'){
+        this.x -= 20;
+    }
+    else if(input === 'right'){
+        this.x += 20;
+    }
+    else if(input === 'up'){
+        this.y -= 20;
+    }
+    else if(input === 'down'){
+        this.y += 20;
+    }
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var bug1 = new Enemy(0, 100, 50);
+var bug2 = new Enemy(0,50, 50);
+var bug3 = new Enemy(0, 200, 40);
+var allEnemies = [bug1, bug2, bug3];
 
-
+var player = new Player(10, 400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
